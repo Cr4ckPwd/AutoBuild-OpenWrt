5addda5eddd11bc630c4016df6a9f8dba938e315
Cr4ckPwd <84202581+Cr4ckPwd@users.noreply.github.com>
2022-07-20 09:40:43 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,6 +5,12 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
+# ADD Custom package
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git base https://github.com/gl-inet/openwrt.git' feeds.conf.default
+sed -i '$a src-git gli_pub https://github.com/gl-inet/gli-pub.git' feeds.conf.default
+sed -i '$a src-git freifunk https://github.com/freifunk/openwrt-packages.git^c621a4283870fe062489fe5e26a15deda7ef4ed7' feeds.conf.default
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
